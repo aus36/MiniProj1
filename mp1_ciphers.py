2cb9161c94a7d6b0e4f75e3b487b4031ee1e1341
aus36 <austin.hale36@gmail.com>
2022-02-10 17:05:34 -0600

    Created menu and input

--- mp1_ciphers.py
+++ mp1_ciphers.py
@@ -1,3 +1,23 @@
+#imports
 import os
 import sys
 from Crypto.Cipher import AES
+
+#function for printing menu items
+def printMenu():
+    print("********************************************************")
+    print("------------- ENCRYPTION/DECRYPTION TOOL ---------------")
+    print("1: AES")
+    print("2: Bruh idk")
+    print("3: lol no idea")
+    usrInput = input("Please select an encryption method from the list above: ")
+    print("********************************************************")
+
+#function for starting and running program
+def startMenu():
+    userInput = ""
+    while(userInput != "quit"):
+        printMenu()
+        userInput = input()
+
+startMenu()
